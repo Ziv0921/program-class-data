95b8d32120307a1974051b478d38d27a64e2ca70
nelsonGX <hi@nelsongx.com>
2025-06-17 13:37:28 +0800

    fix things

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-
 #import tensorflow as tf
 #from tensorflow import keras
 #from tensorflow.keras import layers
@@ -12,9 +11,8 @@ from clean_rank import clean_rank
 
 data = pd.read_csv("files/QS.csv", encoding="latin1")
 
-#delete the wrong data
+#delete & purge the wrong data
 data = data[~data["STATUS"].map(lambda x: isinstance(x, float))]
-
 data["RANK_2025"] = data["RANK_2025"].map(clean_rank)
 
 #delete col we don't need
@@ -36,29 +34,28 @@ data.drop(["International_Research_Network_Rank"], axis="columns", inplace=True)
 data.drop(["Employment_Outcomes_Rank"], axis="columns", inplace=True)
 data.drop(["Sustainability_Rank"], axis="columns", inplace=True)
 
-
-
-data.to_csv('files/output/result.csv')
+# test
+#data.to_csv('files/output/result.csv')
 
 # 6:2:2
-X = data.drop(["RANK_2025"], axis=1) 
+x = data.drop(["RANK_2025"], axis=1) 
 y = data["RANK_2025"]
 
 # 第一次分割
-X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x_train_val, x_test, y_train_val, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 #第二次分割
-X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=42)
+x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val, test_size=0.25, random_state=42)
 
 #檢查比例
-print(f"訓練資料: {len(X_train)} 筆 ({len(X_train)/len(X)*100:.1f}%)")
-print(f"驗證資料: {len(X_val)} 筆 ({len(X_val)/len(X)*100:.1f}%)")
-print(f"測試資料: {len(X_test)} 筆 ({len(X_test)/len(X)*100:.1f}%)")
+print(f"訓練資料: {len(x_train)} 筆 ({len(x_train)/len(x)*100:.1f}%)")
+print(f"驗證資料: {len(x_val)} 筆 ({len(x_val)/len(x)*100:.1f}%)")
+print(f"測試資料: {len(x_test)} 筆 ({len(x_test)/len(x)*100:.1f}%)")
 
 #儲存資料
-X_train.to_csv('files/output/train_features.csv', index=False)
+x_train.to_csv('files/output/train_features.csv', index=False)
 y_train.to_csv('files/output/train_target.csv', index=False)
-X_val.to_csv('files/output/val_features.csv', index=False)
+x_val.to_csv('files/output/val_features.csv', index=False)
 y_val.to_csv('files/output/val_target.csv', index=False)
-X_test.to_csv('files/output/test_features.csv', index=False)
+x_test.to_csv('files/output/test_features.csv', index=False)
 y_test.to_csv('files/output/test_target.csv', index=False)
